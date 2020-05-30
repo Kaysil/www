43204b2311abc2fb01789abeac2b1925e9791d73
Keith <me@keithos.me>
2020-05-30 21:29:57 +1000

    Update new API host

--- ng-stats/src/config/index.js
+++ ng-stats/src/config/index.js
@@ -2,4 +2,4 @@ const isDeveloper = process.env.NODE_ENV === "development"
 
 export const API_HOST = isDeveloper
 	? "http://localhost:5000"
-	: "https://api.nethergames.org"
+	: "https://apiv2.nethergames.org"
